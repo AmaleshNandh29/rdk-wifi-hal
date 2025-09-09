1c62098e7be7b94848432bad007f65afb00c51f9
Amalesh Nandh <Amalesh_Nandh@Comcast.com>
2025-09-09 10:57:04 +0000

    Copy EAPOL Phase

--- src/wifi_hal_hostapd.c
+++ src/wifi_hal_hostapd.c
@@ -3047,6 +3047,7 @@ void update_eapol_sm_params(wifi_interface_info_t *interface)
             eapol_sm_notify_portControl(interface->u.sta.wpa_sm->eapol, Auto);
             wifi_hal_dbg_print("%s:%d:Pramod\n", __func__, __LINE__);
 #endif // CONFIG_WIFI_EMULATOR
+            strncpy(interface->u.sta.wpa_eapol_config.phase2, "auth=MSCHAP", MAX_STR_LEN - 1);
             interface->u.sta.wpa_eapol_method.vendor = EAP_VENDOR_IETF;
             interface->u.sta.wpa_eapol_config.identity = (unsigned char *)identity;
             interface->u.sta.wpa_eapol_config.identity_len = strlen(identity);
